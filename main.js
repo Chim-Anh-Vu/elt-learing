7bda43c350d309776e6c167c0d39099203005801
Chim-Anh-Vu <cotheanhyeuem8668@gmail.com>
2022-08-30 09:03:21 -0700

    Add some function
    create + download

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const {download} = require('electron-dl')
 const path = require('path')
+const fs = require('fs')
+
+let DOWNLOAD_FOLDER = 'DownloadFiles'
+let INSTALL_FOLDER = 'installer'
+let vagrant_msi_url = ''
+let VAGRANT_FILE = 'vagrant_install.msi'
+let vbox_url = ''
+let VBOX_FILE = 'VirtualBox.exe'
+let cent_box = ''
+let CENTOS = 'centos7.box'
+
+let VAGRANT_INSTALL_CMD = ['msiexec /qn /i', 'VAGRANTAPPDIR=', '--norestart']
+let VBOX_INSTALL_CMD = ['--silent', '--ignore-reboot']
+
+
+function createDirectory(directory_name) {
+    if(!fs.existsSync(directory_name)){
+        console.log('Creating folder: '+ directory_name)
+        fs.mkdirSync(directory_name)
+        console.log('Created folder ' + directory_name)
+    }
+}
+
+
+function downloadFile(url, filename) {
+    if(!fs.existsSync(path.join(DOWNLOAD_FOLDER, filename))) {
+        let win = BrowserWindow.getFocusedWindow()
+        download(win, url, {
+            directory: DOWNLOAD_FOLDER,
+            filename: filename
+        })
+    }
+}
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -16,6 +50,8 @@ function createWindow() {
 
     ipcMain.on('activation', (event, title) => {
         console.log(title)
+        createDirectory(DOWNLOAD_FOLDER)
+        createDirectory(INSTALL_FOLDER)
     })
 
     mainWindow.loadFile('index.html')
